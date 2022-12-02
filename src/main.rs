65db92d07fec421de9fae9c2bb5a1771e0916df0
Imre Nagi <imre.nagi@gojek.com>
2022-12-02 15:13:51 -0500

    add calorie counting
    Signed-off-by: Imre Nagi <imre.nagi@gojek.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use crate::year_2022::day01_calorie_counting::calorie_counting;
 pub mod year_2022;
 
 fn main() {
-    let answer = calorie_counting();
+    let answer = calorie_counting("input/2022/day01_calorie_counting.txt");
     println!("{:?}", answer);
 }
--- src/year_2022/day01_calorie_counting.rs
+++ src/year_2022/day01_calorie_counting.rs
@@ -1,4 +1,31 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use std::io;
 
-pub fn calorie_counting() -> u32 {
-  10
+pub fn calorie_counting(path: &str) -> io::Result<i32> {
+
+  let buf_reader = BufReader::new(File::open(path)?);  
+  let mut max = -1;
+  let mut sum = 0;
+  for res in buf_reader.lines() {
+    let line = match res {
+        Ok(l) => l, 
+        Err(_) => panic!("error"),
+    };
+    if line == "" {    
+      if sum > max {        
+        max = sum;
+      }
+      sum = 0;
+
+    } else {
+      let val: i32 = line.parse().unwrap();
+      sum += val;
+    }    
+  }
+  if sum > max {
+    max = sum
+  }  
+  Ok(max)
 }
